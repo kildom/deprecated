0c20e1052427f3c09128e41900e644cbb1f78c58
kildom <kildom@users.noreply.github.com>
2017-05-05 14:06:44 +0200

    Simple test framework and one test suite - test_no_inheritance.js

---
+++ test/test_no_inheritance.js
@@ -0,0 +1,297 @@
+/*
+	-------------------------------------------------------------------------------
+
+	Copyright (c) 2017, Dominik Kilian
+	All rights reserved.
+
+	Redistribution  and  use  in  source  and  binary  forms,   with   or   without
+	modification,  are  permitted  provided  that the following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice,  this
+	   list of conditions and the following disclaimer.
+	2. Redistributions  in  binary  form must reproduce the above copyright notice,
+	   this  list  of  conditions and the following disclaimer in the documentation
+	   and/or other materials provided with the distribution.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+	ANY EXPRESS OR IMPLIED WARRANTIES,  INCLUDING,  BUT NOT LIMITED TO, THE IMPLIED
+	WARRANTIES  OF  MERCHANTABILITY  AND  FITNESS  FOR  A  PARTICULAR  PURPOSE  ARE
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+	ANY DIRECT,  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+	(INCLUDING,  BUT  NOT LIMITED TO,  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	LOSS OF USE,  DATA,  OR PROFITS;  OR BUSINESS INTERRUPTION)  HOWEVER CAUSED AND
+	ON  ANY  THEORY OF LIABILITY,  WHETHER IN CONTRACT,  STRICT LIABILITY,  OR TORT
+	(INCLUDING  NEGLIGENCE OR OTHERWISE)  ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+	-------------------------------------------------------------------------------
+*/
+
+
+
+TEST_CASE('static members', function()
+{	
+	var Class = $class(function(st, pr, pv)
+	{
+		var staticLocal = 'staticLocal';
+		st.pv.staticPrivate = 'staticPrivate';
+		st.pr.staticProtected = 'staticProtected';
+		st.th.staticPublic = 'staticPublic';
+		
+		function loc() { return staticLocal; }
+		st.th.pub = function() { return st.th.staticPublic; }
+		st.pr.pro = function() { return st.pr.staticProtected; }
+		st.pv.prv = function() { return st.pv.staticPrivate; }
+		
+		st.th.testInner = function()
+		{
+			EXPECT_EQ(staticLocal, 'staticLocal');
+			EXPECT_EQ(st.pv.staticPrivate, 'staticPrivate');
+			EXPECT_EQ(st.pr.staticProtected, 'staticProtected');
+			EXPECT_EQ(st.th.staticPublic, 'staticPublic');
+			
+			EXPECT_EQ(loc(), 'staticLocal');
+			EXPECT_EQ(st.pv.prv(), 'staticPrivate');
+			EXPECT_EQ(st.pr.pro(), 'staticProtected');
+			EXPECT_EQ(st.th.pub(), 'staticPublic');
+		}
+		
+		st.th.changeValues = function()
+		{
+			staticLocal = 'lo';
+			st.pv.staticPrivate = 'pv';
+			st.pr.staticProtected = 'pr';
+			st.th.staticPublic = 'th';
+		}
+		
+		st.th.testInnerAfterChange = function()
+		{
+			EXPECT_EQ(staticLocal, 'lo');
+			EXPECT_EQ(st.pv.staticPrivate, 'pv');
+			EXPECT_EQ(st.pr.staticProtected, 'pr');
+			EXPECT_EQ(st.th.staticPublic, 'th');
+			
+			EXPECT_EQ(loc(), 'lo');
+			EXPECT_EQ(st.pv.prv(), 'pv');
+			EXPECT_EQ(st.pr.pro(), 'pr');
+			EXPECT_EQ(st.th.pub(), 'th');
+		}
+	});
+	
+	EXPECT_EQ(typeof(Class.staticLocal), 'undefined');
+	EXPECT_EQ(typeof(Class.staticPrivate), 'undefined');
+	EXPECT_EQ(typeof(Class.staticProtected), 'undefined');
+	EXPECT_EQ(Class.staticPublic, 'staticPublic');
+	
+	EXPECT_EQ(typeof(Class.loc), 'undefined');
+	EXPECT_EQ(typeof(Class.prv), 'undefined');
+	EXPECT_EQ(typeof(Class.pro), 'undefined');
+	EXPECT_EQ(Class.pub(), 'staticPublic');
+	
+	Class.testInner();
+	
+	Class.changeValues();
+	
+	EXPECT_EQ(typeof(Class.staticLocal), 'undefined');
+	EXPECT_EQ(typeof(Class.staticPrivate), 'undefined');
+	EXPECT_EQ(typeof(Class.staticProtected), 'undefined');
+	EXPECT_EQ(Class.staticPublic, 'th');
+	
+	EXPECT_EQ(typeof(Class.loc), 'undefined');
+	EXPECT_EQ(typeof(Class.prv), 'undefined');
+	EXPECT_EQ(typeof(Class.pro), 'undefined');
+	EXPECT_EQ(Class.pub(), 'th');
+	
+	Class.testInnerAfterChange();
+	
+});
+
+
+TEST_CASE('dynamic members', function()
+{
+	var Class = $class(function(st, pr, pv)
+	{
+		return function(th, pr, pv)
+		{
+			var Local = 'Local';
+			pv.Private = 'Private';
+			pr.Protected = 'Protected';
+			th.Public = 'Public';
+			
+			function loc() { return Local; }
+			th.pub = function() { return th.Public; }
+			pr.pro = function() { return pr.Protected; }
+			pv.prv = function() { return pv.Private; }
+			
+			th.testInner = function()
+			{
+				EXPECT_EQ(Local, 'Local');
+				EXPECT_EQ(pv.Private, 'Private');
+				EXPECT_EQ(pr.Protected, 'Protected');
+				EXPECT_EQ(th.Public, 'Public');
+				
+				EXPECT_EQ(loc(), 'Local');
+				EXPECT_EQ(pv.prv(), 'Private');
+				EXPECT_EQ(pr.pro(), 'Protected');
+				EXPECT_EQ(th.pub(), 'Public');
+			}
+			
+			th.changeValues = function()
+			{
+				Local = 'lo';
+				pv.Private = 'pv';
+				pr.Protected = 'pr';
+				th.Public = 'th';
+			}
+			
+			th.testInnerAfterChange = function()
+			{
+				EXPECT_EQ(Local, 'lo');
+				EXPECT_EQ(pv.Private, 'pv');
+				EXPECT_EQ(pr.Protected, 'pr');
+				EXPECT_EQ(th.Public, 'th');
+				
+				EXPECT_EQ(loc(), 'lo');
+				EXPECT_EQ(pv.prv(), 'pv');
+				EXPECT_EQ(pr.pro(), 'pr');
+				EXPECT_EQ(th.pub(), 'th');
+			}
+		};
+	});
+	
+	var obj = new Class();
+	
+	EXPECT_EQ(typeof(obj.Local), 'undefined');
+	EXPECT_EQ(typeof(obj.Private), 'undefined');
+	EXPECT_EQ(typeof(obj.Protected), 'undefined');
+	EXPECT_EQ(obj.Public, 'Public');
+	
+	EXPECT_EQ(typeof(obj.loc), 'undefined');
+	EXPECT_EQ(typeof(obj.prv), 'undefined');
+	EXPECT_EQ(typeof(obj.pro), 'undefined');
+	EXPECT_EQ(obj.pub(), 'Public');
+	
+	obj.testInner();
+	
+	obj.changeValues();
+	
+	EXPECT_EQ(typeof(obj.Local), 'undefined');
+	EXPECT_EQ(typeof(obj.Private), 'undefined');
+	EXPECT_EQ(typeof(obj.Protected), 'undefined');
+	EXPECT_EQ(obj.Public, 'th');
+	
+	EXPECT_EQ(typeof(obj.loc), 'undefined');
+	EXPECT_EQ(typeof(obj.prv), 'undefined');
+	EXPECT_EQ(typeof(obj.pro), 'undefined');
+	EXPECT_EQ(obj.pub(), 'th');
+	
+	obj.testInnerAfterChange();
+});
+
+
+TEST_CASE('static members from dynamic context', function()
+{	
+	var Class = $class(function(st, pr, pv)
+	{
+		var staticLocal = 'staticLocal';
+		st.pv.staticPrivate = 'staticPrivate';
+		st.pr.staticProtected = 'staticProtected';
+		st.th.staticPublic = 'staticPublic';
+		
+		return function(th, pr, pv)
+		{
+			function loc() { return staticLocal; }
+			pv.prv = function() { return st.pv.staticPrivate; }
+			pr.pro = function() { return st.pr.staticProtected; }
+			th.pub = function() { return st.th.staticPublic; }
+			
+			th.changeValues = function()
+			{
+				staticLocal = 'lo';
+				st.pv.staticPrivate = 'pv';
+				st.pr.staticProtected = 'pr';
+				st.th.staticPublic = 'th';
+			}
+			
+			th.testInnerAfterChange = function()
+			{
+				EXPECT_EQ(staticLocal, 'lo');
+				EXPECT_EQ(st.pv.staticPrivate, 'pv');
+				EXPECT_EQ(st.pr.staticProtected, 'pr');
+				EXPECT_EQ(st.th.staticPublic, 'th');
+				
+				EXPECT_EQ(loc(), 'lo');
+				EXPECT_EQ(pv.prv(), 'pv');
+				EXPECT_EQ(pr.pro(), 'pr');
+				EXPECT_EQ(th.pub(), 'th');
+			}
+		};
+	});
+	
+	var obj1 = new Class();
+	var obj2 = new Class();
+	
+	obj1.changeValues();
+	obj2.testInnerAfterChange();
+});
+
+
+TEST_CASE('dynamic members from static context', function()
+{	
+	var Class = $class(function(st, pr, pv)
+	{
+		st.th.testInner = function(obj)
+		{
+			EXPECT_EQ(obj.$(pv).Private, 'Private');
+			EXPECT_EQ(obj.$(pr).Protected, 'Protected');
+			EXPECT_EQ(obj.Public, 'Public');
+			
+			EXPECT_EQ(obj.$(pv).prv(), 'Private');
+			EXPECT_EQ(obj.$(pr).pro(), 'Protected');
+			EXPECT_EQ(obj.pub(), 'Public');
+		}
+		
+		st.th.testInnerAfterChange = function(obj)
+		{
+			EXPECT_EQ(obj.$(pv).Private, 'pv');
+			EXPECT_EQ(obj.$(pr).Protected, 'pr');
+			EXPECT_EQ(obj.Public, 'th');
+			
+			EXPECT_EQ(obj.$(pv).prv(), 'pv');
+			EXPECT_EQ(obj.$(pr).pro(), 'pr');
+			EXPECT_EQ(obj.pub(), 'th');
+		}
+		
+		return function(th, pr, pv)
+		{
+			pv.Private = 'Private';
+			pr.Protected = 'Protected';
+			th.Public = 'Public';
+			
+			th.pub = function() { return th.Public; }
+			pr.pro = function() { return pr.Protected; }
+			pv.prv = function() { return pv.Private; }
+			
+			th.changeValues = function()
+			{
+				pv.Private = 'pv';
+				pr.Protected = 'pr';
+				th.Public = 'th';
+			}
+		};
+	});
+	
+	
+	var obj1 = new Class();
+	var obj2 = new Class();
+	
+	Class.testInner(obj1);
+	Class.testInner(obj2);
+	
+	obj2.changeValues();
+
+	Class.testInner(obj1);
+	Class.testInnerAfterChange(obj2);
+	
+});
+
