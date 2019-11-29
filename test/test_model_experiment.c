83e52cf8a97ac6d72253f5172a971de53e9dee40
kildom <kildom@users.noreply.github.com>
2019-11-29 17:14:16 +0100

    Update test_model_experiment.c

--- test/test_model_experiment.c
+++ test/test_model_experiment.c
@@ -19,9 +19,10 @@ void simuInit(SimuState_t* state)
     state->TESTOBJ__x = 10.0;
 }
 
-void simu(int steps, SimuState_t* state, SimuCallback callback)
+void simu(uint64_t steps, SimuState_t* state, SimuCallback callback)
 {
     // Locals code
+    uint64_t step;
     double time;
     double TESTOBJ__x;
     double TESTOBJ__x__next;
@@ -30,10 +31,12 @@ void simu(int steps, SimuState_t* state, SimuCallback callback)
     double TESTOBJ__y;
 
     // Restore code
+    step = state->step;
+    _stepEnd = step + steps;
     time = state->time;
     TESTOBJ__x = state->TESTOBJ__x;
 
-    while (steps--)
+    for (step = step; step < _stepEnd; step++)
     {
         // Input/output code
         TESTOBJ__U = state->mainU;
@@ -60,16 +63,64 @@ void simu(int steps, SimuState_t* state, SimuCallback callback)
 
 /*
 
+
 class TEST
     state x` = @U - @x
     output y = @U - @x
 
-interface
-    indirect
-    input mainU
-    output mainY = TEST.y
+class INTERFACE
+    struct
+        SimuCallback callback;
+    locals
+        SimuCallback callback;
+    restore
+        callback = state->callback;
+    output mainY
+        @mainY = callback(step, time, @mainU);
 
 TEST
     U = interface.mainU
+
+
+class items:
+    - struct - code to place in state structure
+    - init - code to place in state initialization function
+    - locals - code to place in simulation function local valiables
+    - restore - code to restore locals from state structure
+    - store - code to store locals in the state structure
+    - output [name[,name[,...]]] - code to calculate output data (multiple outputs should be grouped only if needed)
+            names starting with _ are private
+    - output name = code; - short notation of above
+    - provide [name[,name[,...]]] - like output, but does not allocate local variables automatically
+    - override [name[,name[,...]]] - code that overrides symbols with value for the next step
+    - state x - code to calculate next value of the state (using differential or directly)
+    - state x` = code; - short notation of differential
+    - state x# = code; - short notation of direct calculation of next state value
+    - default name - code to calculate input value if it is not connected
+    - init x = code; - short notation of state initialization
+    
+class OBJ1RZ
+    default init = 0.0;
+    init x = @init;
+    state x` = @K * (@U - @x);
+    output y = x
+
+class OBJ1RZ_RAW
+    default init = 0.0;
+    struct
+        double @x;
+    init x = @init;
+    local
+        double @x;
+        double @_x__det;
+        double @_x__next;
+    restore
+        @x = state.@x;
+    provide _x__det = @K * (@U - @x);
+    provide _x__next = @_x__det * dt;
+    override x = @_x__next;
+    store
+        state.@x = @x;
+   
 
 */
