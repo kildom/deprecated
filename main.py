c84452e1710f398b478fdbe253b3c6798862d16c
kildom <kildom@users.noreply.github.com>
2021-02-06 23:59:55 +0100

    Different integer bases, string support and started generation of AST

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import sys
+import re
+import json
 from sly import Lexer, Parser
 import sly
-import sys
 
 class SlyLoggerFilter(object):
     def warning(self, msg, *args, **kwargs):
@@ -12,10 +14,13 @@ class SlyLoggerFilter(object):
 SlyLoggerFilter.old = sly.yacc.SlyLogger.warning
 sly.yacc.SlyLogger.warning = SlyLoggerFilter.warning
 
+
 class UVMSLexer(Lexer):
     _ = _
     tokens = {
-        NAME, NUMBER, IF, ELSE, FUNCTION, DELEGATE, VAR, STRUCT,
+        FP_NUMBER, HEX_NUMBER, OCT_NUMBER, BIN_NUMBER, INV_NUMBER, DEC_NUMBER,
+        STRING, CHAR,
+        NAME, IF, ELSE, FUNCTION, DELEGATE, VAR, STRUCT,
         WHILE, DO, BREAK, CONTINUE, SWITCH, CASE, DEFAULT,
         ENUM, FOR, RETURN, IMPORT, LE, GE, EQ, NE,
         TRY, CATCH, FINALLY, NEW, DELETE, PP, MM, AND, OR, SHL, SHR, THROW, CMP_ASSIGN, XOR }
@@ -63,7 +68,10 @@ class UVMSLexer(Lexer):
     IF = 'if'
     ELSE = 'else'
     NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    STRING = r'"(\\"|[^"])*"'
+    CHAR = r"'(\\'|[^'])*'"
 
+    FP_NUMBER = r'[0-9]*([0-9]\.|\.[0-9])[0-9]*(?:[Ee][+-]?[0-9]+)?|[0-9]+[Ee][+-]?[0-9]+'
     HEX_NUMBER = r'0[Xx][\da-fA-F]+'
     OCT_NUMBER = r'0[Oo][0-7]+'
     BIN_NUMBER = r'0[Bb][01]+'
@@ -73,6 +81,11 @@ class UVMSLexer(Lexer):
     def error(self, t):
         print("Illegal character '%s'" % t.value[0])
         self.index += 1
+
+class NS:
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            self.__dict__[k] = v
 
 class UVMSParser(Parser):
     _ = _
@@ -104,11 +117,12 @@ class UVMSParser(Parser):
 
     @_('module_statement')
     def module(self, p):
-        return p.module_statement
+        return NS(T='Module', items=[p.module_statement])
 
     @_('module module_statement')
     def module(self, p):
-        return f'{p.module}\n{p.module_statement}'
+        p.module.items.append(p.module_statement)
+        return p.module
 
     @_('statement')
     def module_statement(self, p):
@@ -116,15 +130,15 @@ class UVMSParser(Parser):
 
     @_('IMPORT dotted_name ";"')
     def module_statement(self, p):
-        return f'import {p.dotted_name}'
+        return NS(T='Import', module=p.dotted_name, alias=None)
 
     @_('IMPORT NAME "=" dotted_name ";"')
     def module_statement(self, p):
-        return f'import {p.dotted_name} as {p.NAME}'
+        return NS(T='Import', module=p.dotted_name, alias=p.NAME)
 
     @_('empty')
     def params(self, p):
-        return f'[[none]]'
+        return [ ]
 
     @_('params_list')
     def params(self, p):
@@ -132,23 +146,25 @@ class UVMSParser(Parser):
 
     @_('param')
     def params_list(self, p):
-        return p.param
+        return [ p.param ]
 
     @_('params_list "," param')
     def params_list(self, p):
-        return f'{p.params_list} , {p.param}'
+        p.params_list.append(p.param)
+        return p.params_list
 
     @_('typedef NAME')
     def param(self, p):
-        return f'{p.typedef} {p.NAME}'
+        return NS(T='Param', type=p.typedef, name=p.NAME)
 
     @_('NAME %prec DOTTED_NAME_PREC')
     def dotted_name(self, p):
-        return f'{p.NAME}'
+        return [ p.NAME ]
 
     @_('dotted_name "." NAME %prec DOTTED_NAME_PREC')
     def dotted_name(self, p):
-        return f'{p.dotted_name} . {p.NAME}'
+        p.dotted_name.append(p.NAME)
+        return p.dotted_name
 
     @_('dotted_name')
     def typedef(self, p):
@@ -184,47 +200,52 @@ class UVMSParser(Parser):
 
     @_('ENUM NAME "{" enumerators optional_colon "}"')
     def module_statement(self, p):
-        return f'enum {p.NAME} << {p.enumerators} >>'
+        return NS(T='Enum', name=p.NAME, items=p.enumerators)
 
     @_('enumerator')
     def enumerators(self, p):
-        return p.enumerator
+        return [ p.enumerator ]
 
     @_('enumerators "," enumerator')
     def enumerators(self, p):
-        return f'{p.enumerators} ;; {p.enumerator}'
+        p.enumerators.append(p.enumerator)
+        return p.enumerators
 
     @_('NAME enum_value')
     def enumerator(self, p):
-        return f'{p.NAME} = {p.enum_value}'
+        return NS(T='Enumerator', name=p.NAME, value=p.enum_value)
 
     @_('empty')
     def enum_value(self, p):
-        return f'[default]'
-
-    @_('","')
-    def optional_colon(self, p):
-        return " additional ','"
-
-    @_('empty')
-    def optional_colon(self, p):
-        return f''
+        return None
 
     @_('"=" expr')
     def enum_value(self, p):
         return p.expr
 
+    @_('","')
+    def optional_colon(self, p):
+        pass
+
+    @_('empty')
+    def optional_colon(self, p):
+        pass
+
+    @_('FUNCTION typedef NAME "(" params ")" statement')
+    def function(self, p):
+        return NS(T='Function', name=p.NAME, returns=p.typedef, params=p.params, body=p.statement)
+
     @_('FUNCTION NAME "(" params ")" statement')
     def function(self, p):
-        return f'function {p.NAME} ( {p.params} ) {p.statement}'
+        return NS(T='Function', name=p.NAME, returns=None, params=p.params, body=p.statement)
 
     @_('STRUCT NAME parent "{" fields "}"')
     def module_statement(self, p):
-        return f'{p.STRUCT} {p.NAME} ( {p.parent} ) << {p.fields} >>'
+        return NS(T='Struct', union=(p.STRUCT == 'union'), name=p.NAME, parent=p.parent, items=p.fields)
 
     @_('empty')
     def parent(self, p):
-        return ''
+        return None
 
     @_('":" dotted_name')
     def parent(self, p):
@@ -232,15 +253,19 @@ class UVMSParser(Parser):
 
     @_('fields field')
     def fields(self, p):
-        return f'{p.fields} ;; {p.field}'
+        if p.field is not None:
+            p.fields.append(p.field)
+        return p.fields
 
     @_('field')
     def fields(self, p):
-        return p.field
+        if p.field is not None:
+            return [ p.field ]
+        return [ ]
 
     @_('typedef NAME ";"')
     def field(self, p):
-        return f'{p.typedef} {p.NAME}'
+        return NS(T='Field', name=p.NAME, type=p.typedef)
 
     @_('function')
     def field(self, p):
@@ -248,7 +273,7 @@ class UVMSParser(Parser):
 
     @_('STRUCT inner_name parent "{" fields "}"')
     def field(self, p):
-        return f'{p.STRUCT} {p.inner_name} ( {p.parent} ) << {p.fields} >>'
+        return NS(T='InnerStruct', union=(p.STRUCT == 'union'), name=p.inner_name, parent=p.parent, items=p.fields)
 
     @_('NAME')
     def inner_name(self, p):
@@ -256,11 +281,11 @@ class UVMSParser(Parser):
 
     @_('empty')
     def inner_name(self, p):
-        return '[[anonymous]]'
+        return None
 
     @_('";"')
     def field(self, p):
-        return '[[empty]]'
+        return None
 
     @_('statement_without_semicolon')
     def statement(self, p):
@@ -270,57 +295,63 @@ class UVMSParser(Parser):
     def statement(self, p):
         return p.statement_with_semicolon
 
-    @_('TRY "{" block "}" catch_list')
+    @_('TRY "{" block "}" catch_list final')
     def statement_without_semicolon(self, p):
-        return f'try {p.block} {p.catch_list}'
+        return NS(T='Try', body=p.block, catches=p.catch_list, final=p.final)
 
     @_('catch_list catch')
     def catch_list(self, p):
-        return f'{p.catch_list} {p.catch}'
+        p.catch_list.append(p.catch)
+        return p.catch_list
 
     @_('catch')
     def catch_list(self, p):
-        return p.catch
+        return [ p.catch ]
 
     @_('CATCH "(" expr ")" statement')
     def catch(self, p):
-        return f'catch ({p.expr}) {p.statement}'
+        return NS(T='Catch', expr=p.expr, statement=p.statement)
 
     @_('CATCH "(" ")" statement')
     def catch(self, p):
-        return f'catch ([ALL]) {p.statement}'
+        return NS(T='Catch', expr=None, statement=p.statement)
 
     @_('FINALLY statement')
-    def catch(self, p):
-        return f'finally {p.statement}'
+    def final(self, p):
+        return p.statement
+
+    @_('empty')
+    def final(self, p):
+        return None
 
     @_('IF "(" expr ")" statement %prec IFPREC')
     def statement_without_semicolon(self, p):
-        return f'[ if {p.expr} then {p.statement} ]'
+        return NS(T='If', condition=p.expr, then=p.statement, otherwise=None)
 
     @_('IF "(" expr ")" statement ELSE statement')
     def statement_without_semicolon(self, p):
-        return f'[ if {p.expr} then {p.statement0} else {p.statement1} ]'
+        return NS(T='If', condition=p.expr, then=p.statement0, otherwise=p.statement1)
 
     @_('SWITCH "(" expr ")" "{" cases "}"')
     def statement_without_semicolon(self, p):
-        return f'[ switch ({p.expr}) << {p.cases} >> ]'
+        return NS(T='Switch', value=p.expr, cases=p.cases)
 
     @_('case')
     def cases(self, p):
-        return p.case
+        return [ p.case ]
 
     @_('cases case')
     def cases(self, p):
-        return f'{p.cases} ;; {p.case}'
+        p.cases.append(p.case)
+        return p.cases
 
     @_('CASE expr ":" block')
     def case(self, p):
-        return f'case {p.expr}: {p.block}'
+        return NS(T='Case', value=p.expr, body=p.block)
 
     @_('DEFAULT ":" block')
     def case(self, p):
-        return f'default: {p.block}'
+        return NS(T='Case', value=None, body=p.block)
 
     @_('FOR "(" loop_init expr ";" loop_statement ")" statement')
     def statement_without_semicolon(self, p):
@@ -356,7 +387,7 @@ class UVMSParser(Parser):
 
     @_('CONTINUE')
     def statement_with_semicolon(self, p):
-        return f'[ continue ]'
+        return NS(T='Break', breaks=0, contin=True)
 
     @_('THROW expr "," expr')
     def statement_with_semicolon(self, p):
@@ -368,11 +399,11 @@ class UVMSParser(Parser):
 
     @_('break_list CONTINUE')
     def statement_with_semicolon(self, p):
-        return f'[ break {str(p.break_list)} and continue ]'
+        return NS(T='Break', breaks=p.break_list, contin=True)
 
     @_('break_list')
     def statement_with_semicolon(self, p):
-        return f'[ break {str(p.break_list)} ]'
+        return NS(T='Break', breaks=p.break_list, contin=False)
 
     @_('BREAK')
     def break_list(self, p):
@@ -416,7 +447,11 @@ class UVMSParser(Parser):
 
     @_('DELEGATE NAME "(" params ")" ";"')
     def module_statement(self, p):
-        return f'delegate {p.NAME} ( {p.params} )'
+        return NS(T='Delegate', name=p.NAME, returns=None, params=p.params)
+
+    @_('DELEGATE typedef NAME "(" params ")" ";"')
+    def module_statement(self, p):
+        return NS(T='Delegate', name=p.NAME, returns=p.typedef, params=p.params)
 
     @_('"{" block "}"')
     def statement_without_semicolon(self, p):
@@ -598,26 +633,60 @@ class UVMSParser(Parser):
     def brackets(self, p):
         return f'[ ( {p.expr} ) ]'
 
-    @_('NUMBER')
+    @_('HEX_NUMBER')
+    def number(self, p):
+        return str(p.HEX_NUMBER)
+
+    @_('OCT_NUMBER')
+    def number(self, p):
+        return str(p.OCT_NUMBER)
+
+    @_('BIN_NUMBER')
+    def number(self, p):
+        return str(p.BIN_NUMBER)
+
+    @_('DEC_NUMBER')
+    def number(self, p):
+        return str(p.DEC_NUMBER)
+
+    @_('INV_NUMBER')
+    def number(self, p):
+        return 'INVALID'
+
+    @_('FP_NUMBER')
+    def number(self, p):
+        return str(p.FP_NUMBER)
+
+    @_('number')
     def expr(self, p):
-        return str(p.NUMBER)
+        return f'`{p.number}`'
 
     @_('NAME')
     def expr(self, p):
         return f'`{p.NAME}`'
+
+    @_('STRING')
+    def expr(self, p):
+        return f'`{p.STRING}`'
+
+    @_('CHAR')
+    def expr(self, p):
+        return f'`{p.CHAR}`'
 
     def error(*args):
         print(args)
 
 
 text = '''
-x &&= true;
-x.y /= 13;
-x[i] ^= 33 ^^ 2;
-for (a;a;a)
-{
-    aa = 12;
+try {
+    a = 12;
 }
+catch (exception.type == StackOverflowException)
+    log("stack overflow");
+finally
+    log("Done");
+
+
 '''
 
 lexer = UVMSLexer()
@@ -625,3 +694,12 @@ parser = UVMSParser()
 tokens = lexer.tokenize(text)
 tree = parser.parse(tokens)
 print(tree)
+
+
+class Enc(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, NS):
+            return o.__dict__
+        return None
+
+print(json.dumps(tree, cls=Enc, indent='\t'))
