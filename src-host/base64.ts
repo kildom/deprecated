879afef984fd95620616d26b4eaf7219d36ff8ae
kildom <kildom@users.noreply.github.com>
2024-06-24 12:53:23 +0200

    Improve decode base64

--- src-host/base64.ts
+++ src-host/base64.ts
@@ -4,17 +4,18 @@ export function decode(data: string): ArrayBuffer | Promise<ArrayBuffer> {
         return Buffer.from(data, 'base64').buffer;
     } else if (typeof atob === 'function') {
         let binString = atob(data);
-        let bin = new Uint8Array(binString.length);
-        for (let i = 0; i < bin.length; i++) {
-            bin[i] = binString.charCodeAt(i);
-        }
-        return bin.buffer;
+        // Divide into two steps to reduce GUI freeze on slower machines.
+        return new Promise<ArrayBuffer>(resolve => { setTimeout(resolve, 5) })
+            .then(() => {
+                let bin = new Uint8Array(binString.length);
+                for (let i = 0; i < bin.length; i++) {
+                    bin[i] = binString.charCodeAt(i);
+                }
+                return bin.buffer;
+            });
     } else if (typeof fetch === 'function') {
         return fetch('data:application/octet-stream;base64,' + data)
-        .then(res => {
-            return res.arrayBuffer();
-        })
-        .then(arr => arr);
+            .then(res => res.arrayBuffer());
     } else {
         throw new Error('Not implemented');
     }
